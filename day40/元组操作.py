24f88585340501e17dbdf798e06f8b667d3199d1
xiexiaoqian <2320832818@qq.com>
2020-05-04 22:07:30 +0800

    day41 列表高频操作 斐波那契数列 数据样本和清洗 坐标点图

--- day40/元组操作.py
+++ day40/元组操作.py
@@ -10,7 +10,9 @@ b = (1, 'xiaobai', 29.5, '17783208600')
 c = ('001', '2020-05-03', ['三文鱼', '电烤箱'])
 # 从 [1, 5）区间内随机选择 10 个数
 a = random.randint(1, 5, 10)
+print(a)
 # 转 tuple:()
 at = tuple(a)
+print(at)
 # 统计 3 出现次数
 print(at.count(3))
