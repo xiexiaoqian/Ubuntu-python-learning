772cfc7b06be9b17939ac9acc6fb1d3e4cb081a7
xiexiaoqian <2320832818@qq.com>
2020-05-09 00:24:59 +0800

    day45 按行读文件 批量修改文件后缀 批量压缩文件

---
+++ day41-50/day45/按行读文件.py
@@ -0,0 +1,28 @@
+"""
+按行读文件
+@Date 2020.05.08
+"""
+
+from collections import defaultdict
+import re
+
+# 正则，匹配单词
+rec = re.compile(r'\s+')
+# 统计单词出现的频次
+dd = defaultdict(int)
+# 读取文件a.txt, r+ 表示读写模式
+with open('./res/txt/词云.txt', 'r+') as f:
+    # 每次读取一行
+    for line in f:
+        clean_line = line.strip()
+        if clean_line:
+            # 正则分词
+            words = rec.split(clean_line)
+            # 遍历，统计
+            for word in words:
+                dd[word] += 1
+
+# 按照频次降序排列
+dd = sorted(dd.items(), key=lambda x: x[1], reverse=True)
+print('----输出结果-----')
+print(dd)
